861ca11f88920ed6f62f07dd235daa001894fcc4
Bartek Jagniątkowski <git@jagniatkowski.net>
2026-06-02 14:04:54 +0200

    chore: release v1.2.1

--- build.sh
+++ build.sh
@@ -39,9 +39,9 @@ cat > "$APP/Contents/Info.plist" << PLIST
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleVersion</key>
-    <string>1.2.0</string>
+    <string>1.2.1</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.2.0</string>
+    <string>1.2.1</string>
     <key>CFBundleIconFile</key>
     <string>AppIcon</string>
     <key>LSUIElement</key>
